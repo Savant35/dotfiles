896b99afecc8acf4227b3f16ea2ab97e57e544b6
Ali Arthur <aliarthur@Alis-Air.lan>
2023-02-26 17:53:11 -0500

    bash script to setup all files and dependencies

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-cp .vimrc .zshrc .bashrc .tmux.conf .zshenv ~/bashtest
-cd ~/bashtest && mkdir .config || cd .config || mkdir zsh && cd zsh || mkdir plugins || cd plugins && mkdir zsh-autosuggesions zsh-syntax-highlighting;
-mv ~/bashtest zshrc .
+cp .vimrc .zshrc .bashrc .tmux.conf .zshenv aliases ~/
+
+cd ~/
+
+if [ ! -d ".config" ]; then
+  mkdir .config && cd .config && mkdir zsh && mv ~/.zshrc ./zsh;
+  mv ~/aliases ./zsh;
+  cd zsh && mkdir plugins && cd plugins && git clone https://github.com/zsh-users/zsh-autosuggestions && git clone https://github.com/zsh-users/zsh-syntax-highlighting;
+else 
+  cd .config && (mkdir zsh || cd zsh) && mv ~/bashtest/.zshrc ./zsh;
+  mv ~/aliases ./zsh;
+  cd zsh && (mkdir plugins || cd plugins) && git clone https://github.com/zsh-users/zsh-autosuggestions && git clone https://github.com/zsh-users/zsh-syntax-highlighting;
+fi
